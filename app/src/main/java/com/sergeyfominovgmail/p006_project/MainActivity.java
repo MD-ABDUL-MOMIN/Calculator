2b9b29c5e7bf33d5d7822b269b69118bdc284cb3
Sergey Fominov <srgfominov@gmail.com>
2017-02-17 16:49:52 +0400

    add switch

--- app/src/main/java/com/sergeyfominovgmail/p006_project/MainActivity.java
+++ app/src/main/java/com/sergeyfominovgmail/p006_project/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     //описание обьектов для того, чтомы иметь возможность обращаться к ним
     TextView textView;
     Button button1;
-    Button button5;
-    Button button6;
+    Button button2;
+    Button button3;
     Button button7;
     Button button8;
     Button button9;
@@ -30,11 +30,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //инициализация обьектов из нашего layout activity_main
         textView = (TextView) findViewById(R.id.textView);
         button1 = (Button) findViewById(R.id.button1);
-        button5 = (Button) findViewById(R.id.button5);
-        button6 = (Button) findViewById(R.id.button6);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
         button7 = (Button) findViewById(R.id.button7);
         button8 = (Button) findViewById(R.id.button8);
         button9 = (Button) findViewById(R.id.button9);
@@ -46,85 +47,65 @@ public class MainActivity extends AppCompatActivity {
         button14 = (Button) findViewById(R.id.button14);
         button16 = (Button) findViewById(R.id.button16);
 
-        //Традиционный способ, на вход метода сет подается обьект онклик листенер с интерфейсом
-        button1.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText("Enter BuTTON 1");
+                switch (v.getId()){
+                    case R.id.button1:
+                        textView.setText("1");
+                        break;
+                    case R.id.button2:
+                        textView.setText("4");
+                        break;
+                    case R.id.button3:
+                        textView.setText("2");
+                        break;
+                    case R.id.button7:
+                        textView.setText("3");
+                        break;
+                    case R.id.button8:
+                        textView.setText("6");
+                        break;
+                    case R.id.button9:
+                        textView.setText("5");
+                        break;
+                    case R.id.button10:
+                        textView.setText("7");
+                        break;
+                    case R.id.button11:
+                        textView.setText("8");
+                        break;
+                    case R.id.button12:
+                        textView.setText("9");
+                        break;
+                    case R.id.button13:
+                        textView.setText("+ -");
+                        break;
+                    case R.id.button14:
+                        textView.setText("*");
+                        break;
+                    case R.id.button15:
+                        textView.setText("=");
+                        break;
+                    case R.id.button16:
+                        textView.setText("10");
+                        break;
+                }
             }
-        });
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON 4");
-            }
-        });
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON 2");
-            }
-        });
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON 3");
-            }
-        });
-        button8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON 6");
-            }
-        });
-        button9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON 5");
-            }
-        });
-        button10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON 7");
-            }
-        });
-        button11.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON 8");
-            }
-        });
-        button12.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON 9");
-            }
-        });
-        button13.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON + -");
-            }
-        });
-        button14.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON *");
-            }
-        });
-        button15.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON =");
-            }
-        });
-        button16.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText("Enter BuTTON 10");
-            }
-        });
+        };
 
+        button1.setOnClickListener(onClickListener);
+        button2.setOnClickListener(onClickListener);
+        button3.setOnClickListener(onClickListener);
+        button7.setOnClickListener(onClickListener);
+        button8.setOnClickListener(onClickListener);
+        button9.setOnClickListener(onClickListener);
+        button10.setOnClickListener(onClickListener);
+        button11.setOnClickListener(onClickListener);
+        button12.setOnClickListener(onClickListener);
+        button13.setOnClickListener(onClickListener);
+        button14.setOnClickListener(onClickListener);
+        button15.setOnClickListener(onClickListener);
+        button16.setOnClickListener(onClickListener);
     }
 }
