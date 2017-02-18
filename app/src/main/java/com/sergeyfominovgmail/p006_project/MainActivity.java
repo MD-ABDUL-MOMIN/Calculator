bfdacc4847ab90551d0deae33e2b2a182848b1b0
Sergey Fominov <srgfominov@gmail.com>
2017-02-18 12:06:02 +0400

    add setTest and class BtnClickHandler

--- app/src/main/java/com/sergeyfominovgmail/p006_project/MainActivity.java
+++ app/src/main/java/com/sergeyfominovgmail/p006_project/MainActivity.java
@@ -7,16 +7,50 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //вызов черной надписи на ~ 2 секунды, оповещение типа.. прочитать про toast
+//        final Button button7 = (Button) findViewById(R.id.button7);
+//        button7.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View view){
+//                Toast.makeText(getApplicationContext(),"Java 7 button clicked C??" + ((Button)view).getText(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        //это понятно
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.relativeLayoutCalcular);
+        //getChildCount() я так понял, что он возвращает количество подвидов в нашем инте count
+        int count = relativeLayout.getChildCount();
+        //потом мы обрабатываем класс private BCH
+        ButtonClickHandler buttonClickHandler = new ButtonClickHandler();
+        for(int i=0; i<count; i++){
+            //здесь 
+            View v = relativeLayout.getChildAt(i);
+            if(v instanceof Button){
+                v.setOnClickListener(buttonClickHandler);
+            }
+        }
+    }
+
+    private class ButtonClickHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View view){
+            if (view instanceof Button){
+                Button buttonClicked = (Button)view;
+                TextView textViewOutputScreen = (TextView) findViewById(R.id.textView);
+
+                textViewOutputScreen.setText(textViewOutputScreen.getText().toString()+buttonClicked.getText());
+            }
+        }
     }
 }
