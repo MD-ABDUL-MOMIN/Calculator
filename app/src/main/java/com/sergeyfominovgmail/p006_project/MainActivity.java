5ccb578bcbadec0bf208f7518ee6d5d870c023ca
Sergey Fominov <srgfominov@gmail.com>
2017-02-21 18:26:31 +0400

    modification v.2, added simple code

--- app/src/main/java/com/sergeyfominovgmail/p006_project/MainActivity.java
+++ app/src/main/java/com/sergeyfominovgmail/p006_project/MainActivity.java
@@ -2,31 +2,78 @@ package com.sergeyfominovgmail.p006_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnPlus, btnMinus, btnUmn, btnDelit;
+    EditText number1, number2;
+
+    TextView Result;
+    String oper="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPlus = (Button) findViewById(R.id.btnPlus);
+        btnMinus = (Button) findViewById(R.id.btnMinus);
+        btnUmn = (Button) findViewById(R.id.btnUmn);
+        btnDelit = (Button) findViewById(R.id.btnDelit);
 
+        number1 = (EditText) findViewById(R.id.number1);
+        number2 = (EditText) findViewById(R.id.number2);
+
+        Result = (TextView) findViewById(R.id.Result);
+
+        btnPlus.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        btnUmn.setOnClickListener(this);
+        btnDelit.setOnClickListener(this);
     }
 
-    private class ButtonClickHandler implements View.OnClickListener {
-        @Override
-        public void onClick(View view) {
+    @Override
+    public void onClick(View v) {
 
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
 
+        if(TextUtils.isEmpty(number1.getText().toString()) || TextUtils.isEmpty(number2.getText().toString())){
+            return;
         }
+
+        num1 = Float.parseFloat(number1.getText().toString());
+        num2 = Float.parseFloat(number2.getText().toString());
+
+        switch (v.getId()){
+            case R.id.btnPlus:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.btnMinus:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.btnUmn:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.btnDelit:
+                oper = "/";
+                result = num1 / num2;
+                break;
+        }
+
+        Result.setText(num1 + " " + oper + " " + num2 + "=" + result);
     }
-
 }
-
