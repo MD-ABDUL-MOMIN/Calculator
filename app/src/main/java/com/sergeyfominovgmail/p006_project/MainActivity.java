b48320c8edbb406360ccfd9d8f77f46bce7af65d
Sergey Fominov <srgfominov@gmail.com>
2017-02-22 22:19:41 +0400

    tryed, but not working

--- app/src/main/java/com/sergeyfominovgmail/p006_project/MainActivity.java
+++ app/src/main/java/com/sergeyfominovgmail/p006_project/MainActivity.java
@@ -13,113 +13,82 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
-    Button btnPlus, btnMinus, btnUmn, btnDelit, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8;
-    Button btn9;
-    EditText number1, number2;
-    public MainActivity btnClick;
+    EditText number1;
+    private ButtonClickListener btnClick;
+    private String operation;
+    private float nBF;
 
     TextView Result, numbers;
-    String oper="";
+    String oper = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnPlus = (Button) findViewById(R.id.btnPlus);
-        btnMinus = (Button) findViewById(R.id.btnMinus);
-        btnUmn = (Button) findViewById(R.id.btnUmn);
-        btnDelit = (Button) findViewById(R.id.btnDelit);
-
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn4 = (Button) findViewById(R.id.btn4);
-        btn5 = (Button) findViewById(R.id.btn5);
-        btn6 = (Button) findViewById(R.id.btn6);
-        btn7 = (Button) findViewById(R.id.btn7);
-        btn8 = (Button) findViewById(R.id.btn8);
-        btn9 = (Button) findViewById(R.id.btn9);
-
         number1 = (EditText) findViewById(R.id.number1);
-        number2 = (EditText) findViewById(R.id.number2);
-
-        Result = (TextView) findViewById(R.id.Result);
-        numbers = (TextView) findViewById(R.id.numbers);
-
-        btnPlus.setOnClickListener(this);
-        btnMinus.setOnClickListener(this);
-        btnUmn.setOnClickListener(this);
-        btnDelit.setOnClickListener(this);
-
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
-        btn6.setOnClickListener(this);
-        btn7.setOnClickListener(this);
-        btn8.setOnClickListener(this);
-        btn9.setOnClickListener(this);
-
-        int idListbtn[] = {
-                R.id.btn1,
-                R.id.btn2,
-                R.id.btn3,
-                R.id.btn4,
-                R.id.btn5,
-                R.id.btn6,
-                R.id.btn7,
-                R.id.btn8,
-                R.id.btn9
+        int idListbtn[] = {R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6,
+                R.id.btn7, R.id.btn8, R.id.btn9, R.id.btnPlus, R.id.btnMinus, R.id.btnUmn,
+                R.id.btnDelit, R.id.buttonEq
         };
-        for(int id:idListbtn){
+        for (int id : idListbtn) {
             View v = (View) findViewById(id);
             v.setOnClickListener(btnClick);
         }
     }
-
-    @Override
-    public void onClick(View v) {
-
-        float num1 = 0;
-        float num2 = 0;
-        float result = 0;
-
-        if(TextUtils.isEmpty(number1.getText().toString()) || TextUtils.isEmpty(number2.getText().toString())){
-            return;
-        }
-        num1 = Float.parseFloat(number1.getText().toString());
-        num2 = Float.parseFloat(number2.getText().toString());
-
-        switch (v.getId()){
-            case R.id.btnPlus:
-                oper = "+";
-                result = num1 + num2;
-                break;
-            case R.id.btnMinus:
-                oper = "-";
-                result = num1 - num2;
-                break;
-            case R.id.btnUmn:
-                oper = "*";
-                result = num1 * num2;
-                break;
-            case R.id.btnDelit:
-                oper = "/";
-                result = num1 / num2;
-                break;
-            default:
-                String numb = ((Button) v).getText().toString();
-                getKeyboard(numb);
-                break;
-        }
-        Result.setText(num1 + " " + oper + " " + num2 + "=" + result);
+    public void getKeyboard(String str) {
+        String scrC = numbers.getText().toString();
+        scrC += str;
+        numbers.setText(scrC);
     }
-    public void getKeyboard(String str){
-        String scrCurrent = number1.getText().toString();
-        scrCurrent += number1;
-        number1.setText(scrCurrent);
+    public void mResult(){
+        float num = Float.parseFloat(numbers.getText().toString());
+        float result = 0;
+        if(operation.equals("+")){
+            result = num + nBF;
+        }
+        if(operation.equals("-")){
+            result = num - nBF;
+        }
+        if(operation.equals("*")){
+            result = num * nBF;
+        }
+        if(operation.equals("/")){
+            result = num / nBF;
+        }
+        number1.setText(String.valueOf(result));
+    }
+    public void mMath(String str){
+        //save the screen
+        nBF = Float.parseFloat(number1.getText().toString());
+        //operation
+        operation = str;
+        //clear screen
+        number1.setText("0");
+    }
+    public class ButtonClickListener implements View.OnClickListener{
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnPlus:
+                    mMath("+");
+                    break;
+                case R.id.btnMinus:
+                    mMath("-");
+                    break;
+                case R.id.btnUmn:
+                    mMath("*");
+                    break;
+                case R.id.btnDelit:
+                    mMath("/");
+                    break;
+                case R.id.buttonEq:
+                    mResult();
+                default:
+                    String strj = ((Button) v).getText().toString();
+                    getKeyboard(strj);
+                    break;
+            }
+        }
     }
 }
